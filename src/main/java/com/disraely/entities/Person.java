64536793044f07a05ce10b3e0dee857f71b92148
disraelyp <disraely1911@gmail.com>
2023-02-07 22:19:10 -0400

    Create Person abstract class

---
+++ src/main/java/com/disraely/entities/Person.java
@@ -0,0 +1,53 @@
+package com.disraely.entities;
+
+public abstract class Person extends BaseEntity {
+    private String name;
+    private String lastName;
+    private String identification;
+    private String phone;
+    private String address;
+
+    public Person() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
