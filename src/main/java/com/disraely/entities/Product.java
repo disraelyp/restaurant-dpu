52a8e6365bdf8e5b283b1758428d76084dfb9898
disraelyp <disraely1911@gmail.com>
2023-02-09 16:30:01 -0400

    Create Product class

---
+++ src/main/java/com/disraely/entities/Product.java
@@ -0,0 +1,57 @@
+package com.disraely.entities;
+
+import com.disraely.enums.ProductCategory;
+
+import java.util.List;
+
+public class Product extends BaseEntity {
+    private String name;
+    private String description;
+    private Double price;
+    private ProductCategory category;
+    private List<ProductIngredient> productIngredientList;
+
+    public Product() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    public List<ProductIngredient> getProductIngredientList() {
+        return productIngredientList;
+    }
+
+    public void setProductIngredientList(List<ProductIngredient> productIngredientList) {
+        this.productIngredientList = productIngredientList;
+    }
+}
---
+++ src/main/java/com/disraely/enums/ProductCategory.java
@@ -0,0 +1,8 @@
+package com.disraely.enums;
+
+public enum ProductCategory {
+    DRINK,
+    APPETIZER,
+    DESSERT,
+    ENTREE,
+}
